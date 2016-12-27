6b4e733e69714611d0f60fa3a090a8e658698030
Kohei Morita <yosupo06@gmail.com>
2016-12-27 17:27:55 +0900

    fix binsearch

--- source/dcomp/algorithm.d
+++ source/dcomp/algorithm.d
@@ -1,13 +1,11 @@
 module dcomp.algorithm;
 
 //[0,0,0,...,1,1,1]で、初めて1となる場所を探す。pred(l) == 0, pred(r) == 1と仮定
-template binSearch(alias pred) {
-    T binSearch(T)(T l, T r) {
-        while (r-l > 1) {
-            int md = (l+r)/2;
-            if (!pred(md)) l = md;
-            else r = md;
-        }
-        return r;
+T binSearch(alias pred, T)(T l, T r) {
+    while (r-l > 1) {
+        int md = (l+r)/2;
+        if (!pred(md)) l = md;
+        else r = md;
     }
+    return r;
 }
