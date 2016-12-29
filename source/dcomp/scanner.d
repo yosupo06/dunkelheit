1a09db4525eb1c46561a5c395ab9fa7c7a7ed74e
Kohei Morita <yosupo06@gmail.com>
2016-12-29 22:17:33 +0900

    change scanner and add unittest

--- source/dcomp/scanner.d
+++ source/dcomp/scanner.d
@@ -5,32 +5,75 @@ class Scanner {
     import std.conv : to;
     import std.range : front, popFront, array, ElementType;
     import std.array : split;
-    import std.traits : isSomeString, isDynamicArray;
+    import std.traits : isSomeChar, isStaticArray, isArray; 
     import std.algorithm : map;
     File f;
     this(File f = stdin) {
         this.f = f;
     }
     string[] buf;
-    bool succ() {
+    private bool succ() {
         while (!buf.length) {
             if (f.eof) return false;
             buf = f.readln.split;
         }
         return true;
     }
-    int read(Args...)(auto ref Args args) {
-        foreach (i, ref v; args) {
-            if (!succ()) return i;
-            alias VT = typeof(v);
-            static if (!isSomeString!VT && isDynamicArray!VT) {
-                v = buf.map!(to!(ElementType!VT)).array;
-                buf.length = 0;
+    private bool readSingle(T)(ref T x) {
+        if (!succ()) return false;
+        static if (isArray!T) {
+            alias E = ElementType!T;
+            static if (isSomeChar!E) {
+                //string or char[10] etc
+                x = buf.front;
+                buf.popFront;
             } else {
-                v = buf.front.to!VT;
-                buf.popFront();
+                static if (isStaticArray!T) {
+                    //static
+                    assert(buf.length == T.length);
+                }
+                x = buf.map!(to!E).array;
+                buf.length = 0;                
             }
+        } else {
+            x = buf.front.to!T;
+            buf.popFront;            
         }
-        return args.length;
+        return true;
     }
+    int read(T, Args...)(ref T x, auto ref Args args) {
+        if (!readSingle(x)) return 0;
+        static if (args.length == 0) {
+            return 1;
+        } else {
+            return 1 + read(args);
+        }
+    }
+}
+
+unittest {
+    import std.path : buildPath;
+    import std.file : tempDir;
+    import std.algorithm : equal;
+    import std.stdio : File;
+    string fileName = buildPath(tempDir, "kyuridenanmaida.txt");
+    auto fout = File(fileName, "w");
+    fout.writeln("1 2 3");
+    fout.writeln("ab cde");
+    fout.writeln("1.0 1.0 2.0");
+    fout.close;
+    Scanner sc = new Scanner(File(fileName, "r"));
+    int a;
+    int[2] b;
+    char[2] c;
+    string d;
+    double e;
+    double[] f;
+    sc.read(a, b, c, d, e, f);
+    assert(a == 1);
+    assert(equal(b[], [2, 3]));
+    assert(equal(c[], "ab"));
+    assert(equal(d, "cde"));
+    assert(e == 1.0);
+    assert(equal(f, [1.0, 2.0]));
 }
