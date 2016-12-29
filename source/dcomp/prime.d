a6df4c4fcb3b7d717f636cf1e3edf2865ece83de
Kohei Morita <yosupo06@gmail.com>
2016-12-29 22:16:41 +0900

    add unittest to prime

--- source/dcomp/prime.d
+++ source/dcomp/prime.d
@@ -13,9 +13,17 @@ T[] divisorList(T)(T x) {
     return res;
 }
 
+unittest {
+    import std.range, std.algorithm;
+    assert(equal(divisorList(1), [1]));
+    assert(equal(divisorList(2), [1, 2]));
+    assert(equal(divisorList(4), [1, 2, 4]));
+    assert(equal(divisorList(24), [1, 2, 3, 4, 6, 8, 12, 24]));
+}
+
 T[] factorList(T)(T x) {
     T[] res;
-    for (T i = 1; i*i <= x; i++) {
+    for (T i = 2; i*i <= x; i++) {
         while (x % i == 0) {
             res ~= i;
             x /= i;
@@ -24,4 +32,12 @@ T[] factorList(T)(T x) {
     if (x > 1) res ~= x;
     // res is sorted!
     return res;
+}
+
+unittest {
+    import std.range, std.algorithm;
+    assert(equal(factorList(1), new int[0]));
+    assert(equal(factorList(2), [2]));
+    assert(equal(factorList(4), [2, 2]));
+    assert(equal(factorList(24), [2, 2, 2, 3]));
 }
