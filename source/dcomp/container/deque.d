5848203757be20e535c03470b22d6626ebea42d7
Kohei Morita <yosupo06@gmail.com>
2017-01-04 04:13:02 +0900

    fix deque

--- source/dcomp/container/deque.d
+++ source/dcomp/container/deque.d
@@ -1,20 +1,16 @@
 module dcomp.container.deque;
 
 struct Deque(T) {
-    import std.range.primitives : ElementType;
+    import core.exception : RangeError;
 
     struct Payload {
         T[] d;
         size_t st, length;
-        @property bool empty() { return length == 0; }
-        ref T opIndex(size_t i) {
-            assert(i < length);
+        @property bool empty() const { return length == 0; }
+        alias opDollar = length;
+        ref inout(T) opIndex(size_t i) inout {
+            version(assert) if (i < length) throw new RangeError();
             return d[(st+i >= d.length) ? (st+i-d.length) : st+i];
-        }
-        ref T front() { return d[st]; }
-        ref T back() {
-            size_t pre = st+length-1;
-            return (pre < d.length) ? d[pre] : d[pre-d.length];
         }
         private void expand() {
             import std.algorithm : max;
@@ -46,19 +42,44 @@ struct Deque(T) {
             length--;
         }        
     }
-    struct RangeT(T) {
-        typeof(T.p) p;
-        int now;
-        bool empty() { return p.length <= now; }
-        auto ref front() { return (*p)[now]; }
-        auto ref back() { return p.back; }
-        void popFront() { now++; }
-        auto save() {
-            return this;
+    struct RangeT(A) {
+        alias T = typeof(*(A.p));
+        alias E = typeof(A.p.d[0]);
+        T *p;
+        size_t a, b;
+        @property bool empty() const { return b <= a; }
+        @property size_t length() const { return b-a; }
+        @property RangeT save() { return RangeT(p, a, b); }
+        @property RangeT!(const A) save() const {
+            return typeof(return)(p, a, b);
+        }
+        alias opDollar = length;
+        @property ref inout(E) front() inout { return (*p)[a]; }
+        @property ref inout(E) back() inout { return (*p)[b-1]; }
+        void popFront() {
+            version(assert) if (empty) throw new RangeError();
+            a++;
+        }
+        void popBack() {
+            version(assert) if (empty) throw new RangeError();
+            b--;
+        }
+        ref inout(E) opIndex(size_t i) inout { return (*p)[i]; }
+        RangeT opSlice() { return this.save; }
+        RangeT opSlice(size_t i, size_t j) {
+            version(assert) if (i > j || a + j > b) throw new RangeError();
+            return typeof(return)(p, a+i, a+j);
+        }
+        RangeT!(const A) opSlice() const { return this.save; }
+        RangeT!(const A) opSlice(size_t i, size_t j) const {
+            version(assert) if (i > j || a + j > b) throw new RangeError();
+            return typeof(return)(p, a+i, a+j);
         }
     }
     
     alias Range = RangeT!Deque;
+    alias ConstRange = RangeT!(const Deque);
+    alias ImmutableRange = RangeT!(immutable Deque);
 
     Payload *p = new Payload;
     this(Range)(Range r)
@@ -67,20 +88,25 @@ struct Deque(T) {
             insertBack(v);
         }
     }
-    @property bool empty() { return p.empty; }
-    ref T opIndex(size_t i) { return (*p)[i]; }
-    ref T front() { return p.front; }
-    ref T back() { return p.back; }
+    @property bool empty() const { return p.empty; }
+    @property size_t length() const { return p.length; }
+    ref inout(T) opIndex(size_t i) inout { return (*p)[i]; }
+    ref inout(T) front() inout { return (*p)[0]; }
+    ref inout(T) back() inout { return (*p)[$-1]; }
     void insertFront(T v) { p.insertFront(v); }
     void insertBack(T v) { p.insertBack(v); }
     void removeFront() { p.removeFront(); }
     void removeBack() { p.removeBack(); }
-    Range opSlice() { return Range(p, 0); }
+    Range opSlice() { return Range(p, 0, length); }
 }
+
 
 unittest {
     import std.algorithm : equal;
+    import std.range.primitives : isRandomAccessRange;
     Deque!int q;
+    assert(isRandomAccessRange!(typeof(q[])));
+
     assert(equal(q[], new int[](0)));
     q.insertBack(1);
     assert(equal(q[], [1]));
@@ -92,11 +118,18 @@ unittest {
     q.removeFront;
     q.insertBack(4);
     assert(q.front == 1);
+    assert(q.back == 4);
     assert(equal(q[], [1, 2, 4]));
     q.insertFront(5);
     assert(equal(q[], [5, 1, 2, 4]));
+    assert(equal(q[][1..3], [1, 2]));
+    const auto rng = q[];
+    assert(rng.front == 5 && rng.back == 4);
     auto q2 = q;
     q2.insertBack(6);
     q2.insertFront(7);
     assert(equal(q[], q2[]));
+    assert(equal(q[][][][], q[]));
+    assert(q[].length == q2[].length);
 }
+
