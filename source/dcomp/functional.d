fb6a15bf1173788e031cdc63e444cd281565b30a
Kohei Morita <yosupo06@gmail.com>
2017-01-12 11:29:31 +0900

    optimize memoCont

--- source/dcomp/functional.d
+++ source/dcomp/functional.d
@@ -8,47 +8,59 @@ struct memoCont(alias pred) {
     import std.meta;
     alias R = ReturnType!pred;
     alias Args = ParameterTypeTuple!pred;
-    static assert (allSatisfy!(isIntegral, Args)); // should be int only?
-
+    static assert (allSatisfy!(isIntegral, Args));
     static immutable N = Args.length;
-    static string toTuple(string s) { //int[N] -> (N[0], N[1], N[2], ...)
-        return "(" ~ iota(N).map!(i => s~"["~i.to!string~"]").join(",") ~ ")";
-    }
-    static string toArray(string s) { //int[N] -> [N[0]][N[1]][N[2]]...
-        return iota(N).map!(i => "["~s~"["~i.to!string~"]]").join("");
-    }
-    template NArray(T, int N) {
-        static if (!N) alias NArray = T;
-        else alias NArray = NArray!(T, N-1)[];
-    }
     int[2][N] rng;
-    NArray!(R, N) dp;
-    NArray!(bool, N) used;
+    int[N] len;
+    R[] dp;
+    bool[] used;
     void init(int[2][N] rng) {
         this.rng = rng;
-        int[N] len = rng[].map!(a => a[1]-a[0]+1).array;
-        
-        //dp = new typeof(dp)(len[0], len[1], ..., len[N-1])
-        //used = new typeof(used)(len[0], len[1], ..., len[N-1])
-        dp = mixin("new typeof(dp)"~toTuple("len"));
-        used = mixin("new typeof(used)"~toTuple("len"));
+        len = rng[].map!(a => a[1]-a[0]+1).array;
+        int sz = len.reduce!"a*b";
+        dp = new R[sz];
+        used = new bool[sz];
     }
     R opCall(Args args) {
-        int[N] idx;
+        int idx, base = 1;
         foreach (i, v; args) {
             assert(rng[i][0] <= v && v <= rng[i][1]);
-            idx[i] = v - rng[i][0];
+            idx += base*(v - rng[i][0]);
+            base *= len[i];
         }
-        //if (used[idx[0]]..[idx[N-1]]) dp[idx[0]]..[idx[N-1]]
-        //used[idx[0]]..[idx[N-1]] = true
-        if (mixin("used"~toArray("idx"))) return mixin("dp"~toArray("idx"));
-        mixin("used"~toArray("idx")) = true;
-
+        if (used[idx]) return dp[idx];
+        used[idx] = true;
         auto r = pred(args);
-
-        //dp[idx[0]]..[idx[N-1]] = r
-        mixin("dp"~toArray("idx")) = r;
-
+        dp[idx] = r;
         return r;
+    }
+}
+
+unittest {
+    import dcomp.numeric.primitive;
+    import dcomp.numeric.modint;
+    alias Mint = ModInt!(10^^9+7);
+    auto fact = factTable!Mint(100);
+    auto iFac = invFactTable!Mint(100);
+    Mint C0(int a, int b) {
+        if (a < 0 || a < b) return Mint(0);
+        return fact[a]*iFac[b]*iFac[a-b];
+    }
+    struct A {
+        static memoCont!C1base C1;
+        static Mint C1base(int a, int b) {
+            if (a == 0) {
+                if (b == 0) return Mint(1);
+                return Mint(0);
+            }
+            if (b < 0) return Mint(0);
+            return C1(a-1, b-1) + C1(a-1, b);
+        }
+    }
+    A.C1.init([[0, 100], [-2, 100]]);
+    foreach (i; 0..100) {
+        foreach (j; 0..100) {
+            assert(C0(i, j) == A.C1(i, j));
+        }
     }
 }
