35088934ed7a9d018536a0a0e1c16fc496a7fe7b
yosupo <yosupo06@gmail.com>
2017-04-27 04:30:45 +0900

    add combine : remove comment / unittest

--- combine/app.d
+++ combine/app.d
@@ -1,6 +1,70 @@
 import std.stdio, std.process, std.exception, std.string, std.algorithm;
+import std.array, std.conv;
+import std.getopt;
+
+import shrink;
 
 string basePath = "";
+
+int main(string[] args) {
+    string inputName;
+    string outputName;
+    bool removeComment;
+    bool removeUnittest;
+    auto rslt = getopt(args,
+        config.required,
+        "input|i", &inputName,
+        config.required,
+        "output|o", &outputName,
+        "c", &removeComment,
+        "u", &removeUnittest,
+        );
+    if (rslt.helpWanted) {
+        defaultGetoptPrinter("dlang source combiner.",
+            rslt.options);
+    }
+
+    auto f = execute(["dub", "list"]);
+    enforce(f.status == 0, "failed execute");
+    foreach (s; f.output.splitLines) {
+        auto l = s.split;
+        if (l.length == 0 || l[0] != "dcomp") continue;
+        basePath = l[2];
+        break;
+    }
+    enforce(basePath != "", "dcomp not found");
+    writeln(basePath);
+
+    string[] imported = enumImport(inputName);
+    writeln(imported);
+
+    auto ouf = File(outputName, "w");
+    foreach (ph; imported) {
+        auto inf = File(ph, "r");
+        auto data = new ubyte[inf.size()];
+        inf.rawRead(data);
+        bool first = (ph == inputName);
+        if (!first) {
+            ouf.writeln("/* IMPORT " ~ ph ~ " */");
+            if (removeComment) {
+                data = data.trimComment;
+            }
+            if (removeUnittest) {
+                data = data.trimUnittest;
+            }
+        }
+        foreach (line; data.map!(to!char).array.splitLines) {
+            if (willCommentOut(line.idup)) {
+                ouf.writeln("// " ~ line);
+            } else {
+                ouf.writeln(line);
+            }
+        }
+    }
+    return 0;
+}
+
+
 string[] line2Imp(string s) {
     string[] res;
     auto l = s.split;
@@ -49,36 +113,3 @@ string[] enumImport(string fn) {
     return res;
 }
 
-int main(string[] argv) {
-    auto f = execute(["dub", "list"]);
-    enforce(f.status == 0, "failed execute");
-    foreach (s; f.output.splitLines) {
-        auto l = s.split;
-        if (l.length == 0 || l[0] != "dcomp") continue;
-        basePath = l[2];
-        break;
-    }
-    enforce(basePath != "", "dcomp not found");
-    writeln(basePath);
-    enforce(argv.length == 3, "usage: dub run dcomp:combine -- [input] [output]");
-
-    string[] imported = enumImport(argv[1]);
-    writeln(imported);
-
-    auto ouf = File(argv[2], "w");
-    foreach (ph; imported) {
-        auto inf = File(ph, "r");
-        if (ph != argv[1]) {
-            ouf.writeln("/* IMPORT " ~ ph ~ " */");
-        }
-        foreach (line; inf.byLine) {
-            if (willCommentOut(line.idup)) {
-                ouf.writeln("// " ~ line);
-            } else {
-                ouf.writeln(line);
-            }
-        }
-    }
-
-    return 0;
-}
