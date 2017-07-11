6778fdf554fd8ac38652cac0a11c2e55c8bf527b
Kohei Morita <yosupo@localhost.localdomain>
2017-07-12 00:44:02 +0900

    fix lazyseg && add method && add unittest

--- source/dcomp/datastructure/lazyseg.d
+++ source/dcomp/datastructure/lazyseg.d
@@ -1,5 +1,6 @@
 module dcomp.datastructure.lazyseg;
 
+/// 遅延評価Segment Tree
 struct LazySeg(T, L, alias opTT, alias opTL, alias opLL, T eT, L eL) {
     const int n, sz, lg;
     T[] d;
@@ -29,6 +30,7 @@ struct LazySeg(T, L, alias opTT, alias opTL, alias opLL, T eT, L eL) {
         lzAdd(2*k+1, lz[k]);
         lz[k] = eL;
     }
+    //d[a]+d[a+1]+...+d[b-1]
     T sum(int a, int b, int l, int r, int k) {
         if (b <= l || r <= a) return eT;
         if (a <= l && r <= b) return d[k];
@@ -43,6 +45,16 @@ struct LazySeg(T, L, alias opTT, alias opTL, alias opLL, T eT, L eL) {
             push(k>>i);
         }
         return d[k];
+    }
+    void singleSet(T x, int k) {
+        k += sz;
+        foreach_reverse (int i; 1..lg+1) {
+            push(k>>i);
+        }
+        d[k] = x;
+        foreach (int i; 1..lg+1) {
+            d[k>>i] = opTT(d[2*(k>>i)], d[2*(k>>i)+1]);
+        }
     }
     T sum(int a, int b) {
         assert(0 <= a && a <= b && b <= n);
@@ -72,6 +84,14 @@ struct LazySeg(T, L, alias opTT, alias opTL, alias opLL, T eT, L eL) {
             return seg.sum(start, end);
         }
     }
+    T opIndex(int k) {
+        assert(0 <= k && k < n);
+        return single(k);
+    }
+    void opIndexAssign(T x, int k) {
+        assert(0 <= k && k < n);
+        singleSet(x, k);
+    }
     int[2] opSlice(size_t dim)(int start, int end) {
         assert(0 <= start && start <= end && end <= sz);
         return [start, end];
@@ -82,6 +102,26 @@ struct LazySeg(T, L, alias opTT, alias opTL, alias opLL, T eT, L eL) {
     void opIndexOpAssign(string op : "+")(L x, int[2] rng) {
         add(rng[0], rng[1], x);
     }
+}
+
+///
+unittest {
+    import std.algorithm : max;
+    auto seg = LazySeg!(int, int,
+        (a, b) => max(a, b), (a, b) => a+b, (a, b) => a+b, 0, 0)(3);
+    
+    //[2, 1, 4]
+    seg[0] = 2; seg[1] = 1; seg[2] = 4;
+    assert(seg[0..3].sum == 4);
+
+    //[2, 1, 5]
+    seg[2] = 5;
+    assert(seg[0..2].sum == 2);
+    assert(seg[0..3].sum == 5);
+
+    //[12, 11, 5]
+    seg[0..2] += 10;
+    assert(seg[0..3].sum == 12);
 }
 
 unittest {
