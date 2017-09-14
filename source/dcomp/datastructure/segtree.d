39638806b49ab2a2317c09b494f0f2a36d643d82
Kohei Morita <yosupo06@gmail.com>
2017-09-15 03:24:04 +0900

    refactor segtree

--- source/dcomp/datastructure/segtree.d
+++ source/dcomp/datastructure/segtree.d
@@ -2,112 +2,112 @@ module dcomp.datastructure.segtree;
 
 /// 遅延伝搬Segment Tree
 struct LazySeg(T, L, alias opTT, alias opTL, alias opLL, T eT, L eL, bool isSimple = false) {
-    const int n, sz, lg;
+    const uint n, sz, lg;
     T[] d;
     static if (!isSimple) L[] lz;
     @disable this();
-    this(int n) {
+    this(uint n) {
         import std.algorithm : fill, each;
         import core.bitop : bsr;
         if (n == 0) return;
-        int lg = n.bsr;
-        if ((2^^lg) < n) lg++;
+        uint lg = 0;
+        while ((2^^lg) < n) lg++;
         this.n = n;
-        this.sz = 2^^lg;
         this.lg = lg;
-        d = new T[](2*this.sz);
+        sz = 2^^lg;
+        d = new T[](2*sz);
         d.each!((ref x) => x = eT);
         static if (!isSimple) {
-            lz = new L[](2*this.sz);
+            lz = new L[](2*sz);
             lz.each!((ref x) => x = eL);
         }
     }
     static if (isSimple) {
-        private void lzAdd(int k, L x) {}
-        private void push(int k) {}
+        private void lzAdd(uint k, L x) {}
+        private void push(uint k) {}
     } else {
-        private void lzAdd(int k, L x) {
+        private void lzAdd(uint k, L x) {
             d[k] = opTL(d[k], x);
             lz[k] = opLL(lz[k], x);
         }
-        private void push(int k) {
+        private void push(uint k) {
             if (lz[k] == eL) return;
             lzAdd(2*k, lz[k]);
             lzAdd(2*k+1, lz[k]);
             lz[k] = eL;
         }
     }
-    //d[a]+d[a+1]+...+d[b-1]
-    T sum(int a, int b, int l, int r, int k) {
-        if (b <= l || r <= a) return eT;
-        if (a <= l && r <= b) return d[k];
-        push(k);
-        int md = (l+r)/2;
-        return opTT(sum(a, b, l, md, 2*k),
-            sum(a, b, md, r, 2*k+1));
-    }
-    T single(int k) {
+    T single(uint k) {
         k += sz;
-        foreach_reverse (int i; 1..lg+1) {
+        foreach_reverse (uint i; 1..lg+1) {
             push(k>>i);
         }
         return d[k];
     }
-    void singleSet(T x, int k) {
+    void singleSet(T x, uint k) {
         k += sz;
-        foreach_reverse (int i; 1..lg+1) {
+        foreach_reverse (uint i; 1..lg+1) {
             push(k>>i);
         }
         d[k] = x;
-        foreach (int i; 1..lg+1) {
+        foreach (uint i; 1..lg+1) {
             d[k>>i] = opTT(d[2*(k>>i)], d[2*(k>>i)+1]);
         }
     }
-    T sum(int a, int b) {
+    //d[a]+d[a+1]+...+d[b-1]
+    T sum(uint a, uint b, uint l, uint r, uint k) {
+        if (b <= l || r <= a) return eT;
+        if (a <= l && r <= b) return d[k];
+        push(k);
+        uint md = (l+r)/2;
+        return opTT(sum(a, b, l, md, 2*k),
+            sum(a, b, md, r, 2*k+1));
+    }    
+    T sum(uint a, uint b) {
         assert(0 <= a && a <= b && b <= n);
         return sum(a, b, 0, sz, 1);
     }
-    void add(int a, int b, L x, int l, int r, int k) {
+    void add(uint a, uint b, L x, uint l, uint r, uint k) {
         if (b <= l || r <= a) return;
         if (a <= l && r <= b) {
             lzAdd(k, x);
             return;
         }
         push(k);
-        int md = (l+r)/2;
+        uint md = (l+r)/2;
         add(a, b, x, l, md, 2*k);
         add(a, b, x, md, r, 2*k+1);
         d[k] = opTT(d[2*k], d[2*k+1]);
     }
-    void add(int a, int b, L x) {
+    void add(uint a, uint b, L x) {
         assert(0 <= a && a <= b && b <= n);
         add(a, b, x, 0, sz, 1);
     }
-    @property int opDollar() const {return sz;}
+    @property uint opDollar() const {return sz;}
     struct Range {
         LazySeg* seg;
-        int start, end;
+        uint start, end;
         @property T sum() {
             return seg.sum(start, end);
         }
     }
-    T opIndex(int k) {
+    T opIndex(uint k) {
         assert(0 <= k && k < n);
         return single(k);
     }
-    void opIndexAssign(T x, int k) {
+    void opIndexAssign(T x, uint k) {
         assert(0 <= k && k < n);
         singleSet(x, k);
     }
-    int[2] opSlice(size_t dim)(int start, int end) {
+    uint[2] opSlice(size_t dim)(uint start, uint end) {
         assert(0 <= start && start <= end && end <= sz);
         return [start, end];
     }
-    Range opIndex(int[2] rng) {
+    Range opIndex(uint[2] rng) {
         return Range(&this, rng[0], rng[1]);
     }
     static if (!isSimple) {
-        void opIndexOpAssign(string op : "+")(L x, int[2] rng) {
+        void opIndexOpAssign(string op : "+")(L x, uint[2] rng) {
             add(rng[0], rng[1], x);
         }
     }
@@ -136,12 +136,14 @@ unittest {
 
 unittest {
     //issue 17466
+    import std.stdio;
     auto seg = LazySeg!(long[2], long[2],
         (a, b) => a, (a, b) => a, (a, b) => a, [0L, 0L], [0L, 0L])(10);
 }
 
-alias SimpleSeg(T, alias op, T e) = LazySeg!(T, bool,op, (a, b) => a, (a, b) => a, e, false, false);
+alias SimpleSeg(T, alias op, T e) = LazySeg!(T, bool, op, (a, b) => a, (a, b) => a, e, false, false);
 
+///
 unittest {
     import std.algorithm : max;
     ///区間加算
