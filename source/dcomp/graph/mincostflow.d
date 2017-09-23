785e9f91f016024505a5c5ef17a3787a4ede8d06
Kohei Morita <yosupo06@gmail.com>
2017-09-23 16:39:24 +0900

    mincostflow: add stress test and many fix

--- source/dcomp/graph/mincostflow.d
+++ source/dcomp/graph/mincostflow.d
@@ -4,20 +4,16 @@ import dcomp.container.deque;
 
 import std.stdio;
 
-struct minCostFlowInfo(C, D, T) {
+struct MinCostFlowInfo(C, D, T) {
     T g; int s, t;
-    D INF, EPS;
-    C capFlow;
-    D flow;
+    C nc, capFlow;
+    D nd, flow;
     D[] dual; //potential
-    C nc; D nd;
     int[] pv, pe; //path
-    this(T g, int s, int t, D EPS, D INF) {
+    this(T g, int s, int t) {
         this.g = g;
         this.s = s;
         this.t = t;
-        this.INF = INF;
-        this.EPS = EPS;
         dual = new D[g.length];
         pv = new int[g.length];
         pe = new int[g.length];
@@ -25,14 +21,13 @@ struct minCostFlowInfo(C, D, T) {
 }
 
 /// 最小費用流
-minCostFlowInfo!(C, D, T) minCostFlow(C, D, T)(T g, int s, int t, D EPS, D INF, bool neg) {
+MinCostFlowInfo!(C, D, T) minCostFlow(C, D, T)(T g, int s, int t, bool neg) {
     assert(s != t);
     import std.algorithm : map;
     import std.range : array;
     import std.conv : to;
     int n = g.length.to!int;
-    auto mcfInfo =
-        minCostFlowInfo!(C, D, T)(g, s, t, EPS, INF);
+    auto mcfInfo = MinCostFlowInfo!(C, D, T)(g, s, t);
     mcfInfo.dualRef(neg);
     return mcfInfo;
 }
@@ -43,6 +38,7 @@ unittest {
     struct Edge {
         int to, cap, dist, rev;
     }
+
     void addEdge(Edge[][] g, int from, int to, int cap, int dist) {
         g[from] ~= Edge(to, cap, dist, g[to].length.to!int);
         g[to] ~= Edge(from, 0, -dist, g[from].length.to!int-1);
@@ -54,16 +50,75 @@ unittest {
     addEdge(g, 0, 2, 15, 3);
     addEdge(g, 1, 3, 3, 2);
     addEdge(g, 2, 3, 20, 4);
-
-    auto mcfInfo = minCostFlow!(int, int)(g, 0, 3, 0, 10^^9, false);
+    
+    auto mcfInfo = minCostFlow!(int, int)(g, 0, 3, false);
     mcfInfo.manyFlow(10^^9);
     assert(mcfInfo.capFlow == 18);
     assert(mcfInfo.flow == 3*(3+2) + 15*(3+4));
 }
 
-C singleFlow(C, D, T)(ref minCostFlowInfo!(C, D, T) mcfInfo, C c) {
+
+unittest {
+    import std.algorithm, std.conv, std.stdio, std.range;
+    import std.random;
+    import std.typecons;
+    import std.datetime;
+
+    struct E {
+        int to, cap, dist, rev;
+    }
+    void addEdge(E[][] g, int from, int to, int cap, int dist) {
+        g[from] ~= E(to, cap, dist, g[to].length.to!int);
+        g[to] ~= E(from, 0, -dist, g[from].length.to!int-1);
+    }
+
+
+    writeln("MinCostFlow Random5000");
+
+    void f() {
+        int n = uniform(2, 20);
+        int m = uniform(0, 200);
+        int s, t;
+        while (true) {
+            s = uniform(0, n);
+            t = uniform(0, n);
+            if (s != t) break;
+        }
+        auto g = new E[][n];
+        E[][] elist = new E[][n];
+
+        foreach (i; 1..m) {
+            int x, y;
+            while (true) {
+                x = uniform(0, n);
+                y = uniform(0, n);
+                if (x == y) continue;
+                break;
+            }
+            int c = uniform(0, 100);
+            int d = uniform(0, 100);
+            addEdge(g, x, y, c, d);
+            elist[x] ~= E(y, c, d, -1);
+        }
+
+        auto res = minCostFlow!(int, int)(g, s, t, false);
+        res.manyFlow(10^^9);
+        int sm = (res.dual[t]-res.dual[s]) * res.capFlow;
+        foreach (i, v; elist) {
+            foreach (e; v) {
+                sm -= max(0, (res.dual[e.to] - res.dual[i]) - e.dist) * e.cap;
+            }
+        }
+        assert(res.flow == sm);
+    }
+    auto ti = benchmark!(f)(5000);
+    writeln(ti[0].msecs, "ms");
+}
+
+C singleFlow(C, D, T)(ref MinCostFlowInfo!(C, D, T) mcfInfo, C c) {
     import std.algorithm;
     with (mcfInfo) {
+        if (nd == D.max) return nc;
         c = min(c, nc);
         for (int v = t; v != s; v = pv[v]) {
             g[pv[v]][pe[v]].cap -= c;
@@ -77,7 +132,7 @@ C singleFlow(C, D, T)(ref minCostFlowInfo!(C, D, T) mcfInfo, C c) {
     return c;
 }
 
-void manyFlow(C, D, T)(ref minCostFlowInfo!(C, D, T) mcfInfo, C c) {
+void manyFlow(C, D, T)(ref MinCostFlowInfo!(C, D, T) mcfInfo, C c) {
     with (mcfInfo) {
         D res = 0;
         while (c) {
@@ -92,7 +147,7 @@ void manyFlow(C, D, T)(ref minCostFlowInfo!(C, D, T) mcfInfo, C c) {
 
 import dcomp.container.deque;
 
-void dualRef(bool neg, C, D, T)(ref minCostFlowInfo!(C, D, T) mcfInfo) {
+void dualRef(bool neg, C, D, T)(ref MinCostFlowInfo!(C, D, T) mcfInfo) {
     import std.conv : to;
     import std.typecons;
     import std.container;
@@ -105,7 +160,8 @@ void dualRef(bool neg, C, D, T)(ref minCostFlowInfo!(C, D, T) mcfInfo) {
 
     with(mcfInfo) {
         int n = g.length.to!int;
-        D[] dist = new D[n]; dist[] = INF;
+        D[] dist = new D[n]; dist[] = D.max;
+        pv[] = -1; pe[] = -1;
         Deque!int refV;
         auto que = mixin(INIT);
 
@@ -146,6 +202,11 @@ void dualRef(bool neg, C, D, T)(ref minCostFlowInfo!(C, D, T) mcfInfo) {
                 }
             }
         }
+        if (dist[t] == D.max) {
+            nd = D.max;
+            nc = 0;
+            return;
+        }
         static if (!neg) {
             while (!refV.empty()) {
                 int v = refV.back(); refV.removeBack();
@@ -157,25 +218,19 @@ void dualRef(bool neg, C, D, T)(ref minCostFlowInfo!(C, D, T) mcfInfo) {
                 dual[v] += dist[v];
             }
         }
-
-        if (dist[t] == INF) {
-            nd = INF;
-            nc = 0;
-            return;
-        }
+        
         nd = dual[t]-dual[s];
-        nc = INF;
+        nc = C.max;
         for (int v = t; v != s; v = pv[v]) {
             nc = min(nc, g[pv[v]][pe[v]].cap);
         }
     }
 }
 
-void dualRef(C, D, T)(ref minCostFlowInfo!(C, D, T) mcfInfo, bool neg) {
+void dualRef(C, D, T)(ref MinCostFlowInfo!(C, D, T) mcfInfo, bool neg) {
     if (neg == false) {
         dualRef!false(mcfInfo);
     } else {
         dualRef!true(mcfInfo);
     }
 }
-
