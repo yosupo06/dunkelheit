911328cf8cd0eb63d57fead598fb49fedb963f9a
Kohei Morita <yosupo06@gmail.com>
2017-11-07 04:03:47 +0900

    add nft

--- source/dcomp/numeric/convolution.d
+++ source/dcomp/numeric/convolution.d
@@ -144,7 +144,57 @@ unittest {
     writeln("FFT Stress: ", sw.peek.msecs);
 }
 
-import dcomp.modint;
+import dcomp.modint, dcomp.numeric.primitive;
+
+void nft(uint G, bool type, Mint)(Mint[] c) {
+    import std.algorithm : swap;
+    import core.bitop : bsr;    
+    size_t N = c.length;
+    assert(N);
+    size_t S = bsr(N);
+    assert(1<<S == N);
+
+    Mint[] a = c.dup, b = new Mint[N];
+    foreach (i; 1..S+1) {
+        size_t W = 1<<(S-i);
+        Mint base = pow(Mint(G), Mint(-1).v/(1<<i));
+        if (type) base = Mint(1)/base;
+        Mint now = 1;
+        for (size_t y = 0; y < N/2; y += W) {
+            foreach (x; 0..W) {
+                auto l =       a[y<<1 | x];
+                auto r = now * a[y<<1 | x | W];
+                b[y | x]        = l+r;
+                b[y | x | N>>1] = l-r;
+            }
+            now *= base;
+        }
+        swap(a, b);
+    }
+    c[] = a[];    
+}
+
+Mint[] multiply(uint G, Mint)(Mint[] a, Mint[] b) {
+    size_t A = a.length, B = b.length;
+    if (!A || !B) return [];
+    size_t lg = 1;
+    while ((1<<lg) < A+B-1) lg++;
+    size_t N = 1<<lg;
+    Mint[] _a = new Mint[N];
+    Mint[] _b = new Mint[N];
+    foreach (i; 0..A) _a[i] = a[i];
+    foreach (i; 0..B) _b[i] = b[i];
+    nft!(G, false)(_a);
+    nft!(G, false)(_b);
+    foreach (i; 0..N) _a[i] *= _b[i];
+    nft!(G, true)(_a);
+    Mint[] c = new Mint[A+B-1];
+    Mint iN = Mint(1) / Mint(N);
+    foreach (i; 0..A+B-1) {
+        c[i] = _a[i] * iN;
+    }
+    return c;
+}
 
 Mint[] multiply(Mint, size_t M = 3, size_t W = 10)(Mint[] a, Mint[] b)
 if (isModInt!Mint) {
@@ -211,6 +261,42 @@ if (isModInt!Mint) {
 }
 
 unittest {
+    alias Mint = ModInt!924844033;
+    import std.algorithm, std.datetime, std.stdio, std.random, std.math;
+    StopWatch sw; sw.start;
+    Mint rndM() { return Mint(uniform(0, 924844033)); }
+    foreach (L; 1..20) {
+        foreach (R; 1..20) {
+            foreach (ph; 0..10) {
+                Mint[] a = new Mint[L];
+                Mint[] b = new Mint[R];
+                foreach (ref x; a) x = rndM();
+                foreach (ref x; b) x = rndM();
+                Mint[] c1 = multiply!5(a, b);
+                Mint[] c2 = new Mint[L+R-1];
+                foreach (i; 0..L) {
+                    foreach (j; 0..R) {
+                        c2[i+j] += a[i] * b[j];
+                    }
+                }
+                assert(c1.length == c2.length);
+                foreach (i; 0..L+R-1) {
+                    if (c1[i] != c2[i]) {
+                        writeln(a);
+                        writeln(b);
+                        writeln(c1);
+                        writeln(c2);
+                    }
+                    assert(c1[i] == c2[i]);
+                }
+            }
+        }
+    }
+    writeln("NFT Stress: ", sw.peek.msecs);    
+}
+
+
+unittest {
     alias Mint = ModInt!(10^^9 + 7);
     import std.algorithm, std.datetime, std.stdio, std.random, std.math;
     StopWatch sw; sw.start;
@@ -244,3 +330,4 @@ unittest {
     }
     writeln("FFT(ModInt) Stress: ", sw.peek.msecs);    
 }
+
