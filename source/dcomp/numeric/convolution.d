5332751df6ee20d97faec2452204b96e0aba6185
Kohei Morita <yosupo06@gmail.com>
2017-11-07 03:07:38 +0900

    add modint convolution

--- source/dcomp/numeric/convolution.d
+++ source/dcomp/numeric/convolution.d
@@ -143,3 +143,103 @@ unittest {
     }
     writeln("FFT Stress: ", sw.peek.msecs);
 }
+
+import dcomp.modint;
+
+Mint[] multiply(Mint, size_t M = 3, size_t W = 10)(Mint[] a, Mint[] b)
+if (isModInt!Mint) {
+    import std.math : round;
+    alias P = Complex!double;
+
+    size_t A = a.length, B = b.length;
+    if (!A || !B) return [];
+    auto N = A + B - 1;
+    size_t lg = 1;
+    while ((1<<lg) < N) lg++;
+    N = 1<<lg;
+
+    P[][M] x, y;
+    foreach (ph; 0..M) {
+        x[ph] = new P[N];
+        y[ph] = new P[N];
+        P[] z = new P[N]; z[] = P(0, 0);
+        foreach (i; 0..A) z[i].re = (a[i].v >> (ph*W)) % (1<<W);
+        foreach (i; 0..B) z[i].im = (b[i].v >> (ph*W)) % (1<<W);
+        fft!false(z);
+        foreach (i; 0..N) z[i] *= 0.5;
+        foreach (i; 0..N) {
+            auto j = i ? N-i : 0;
+            x[ph][i] = P(z[i].re+z[j].re,  z[i].im-z[j].im);
+            y[ph][i] = P(z[i].im+z[j].im, -z[i].re+z[j].re);
+        }
+    }
+    P[][M] z;
+    foreach (i; 0..M) {
+        z[i] = new P[N]; z[i][] = P(0, 0);
+    }
+    foreach (af; 0..M) {
+        foreach (bf; 0..M) {
+            auto cf = af+bf;
+            if (cf >= M) cf -= M;
+            foreach (i; 0..N) {
+                if (af + bf < M) {
+                    z[cf][i] += x[af][i]*y[bf][i];
+                } else {
+                    z[cf][i] += x[af][i]*y[bf][i]*P(0, 1);
+                }
+            }
+        }
+    }
+    foreach (i; 0..M) fft!true(z[i]);
+
+    Mint[] c = new Mint[A+B-1];
+    Mint base = 1;
+    foreach (ph; 0..2*M-1) {
+        foreach (i; 0..A+B-1) {
+            if (ph < M) {
+                z[ph][i].re *= 1.0/N;
+                c[i] += Mint(cast(long)(round(z[ph][i].re)))*base;
+            } else {       
+                z[ph-M][i].im *= 1.0/N;
+                c[i] += Mint(cast(long)(round(z[ph-M][i].im)))*base;
+            }
+        }
+        base *= Mint(1<<W);
+    }
+    return c;
+}
+
+unittest {
+    alias Mint = ModInt!(10^^9 + 7);
+    import std.algorithm, std.datetime, std.stdio, std.random, std.math;
+    StopWatch sw; sw.start;
+    Mint rndM() { return Mint(uniform(0, 10^^9 + 7)); }
+    foreach (L; 1..20) {
+        foreach (R; 1..20) {
+            foreach (ph; 0..10) {
+                Mint[] a = new Mint[L];
+                Mint[] b = new Mint[R];
+                foreach (ref x; a) x = rndM();
+                foreach (ref x; b) x = rndM();
+                Mint[] c1 = multiply(a, b);
+                Mint[] c2 = new Mint[L+R-1];
+                foreach (i; 0..L) {
+                    foreach (j; 0..R) {
+                        c2[i+j] += a[i] * b[j];
+                    }
+                }
+                assert(c1.length == c2.length);
+                foreach (i; 0..L+R-1) {
+                    if (c1[i] != c2[i]) {
+                        writeln(a);
+                        writeln(b);
+                        writeln(c1);
+                        writeln(c2);
+                    }
+                    assert(c1[i] == c2[i]);
+                }
+            }
+        }
+    }
+    writeln("FFT(ModInt) Stress: ", sw.peek.msecs);    
+}
