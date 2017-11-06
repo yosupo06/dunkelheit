43c7592d14f1f5e30694d8a5f8cc3c3a5fc023b9
Kohei Morita <yosupo06@gmail.com>
2017-11-07 01:56:42 +0900

    opt memory

--- source/dcomp/numeric/convolution.d
+++ source/dcomp/numeric/convolution.d
@@ -41,29 +41,42 @@ T[] hadamard(T)(T[] v, bool rev) {
 
 import std.complex;
 
+double[] fftSinList(size_t S) {
+    import std.math : PI, sin;
+    assert(2 <= S);
+    size_t N = 1<<S;
+    static double[][30] buf;
+    if (!buf[S].length) {
+        buf[S] = new double[3*N/4+1];
+        foreach (i; 0..N/4+1) {
+            buf[S][i] = sin(i*2*double(PI)/N);
+            buf[S][N/2-i] = buf[S][i];
+            buf[S][N/2+i] = -buf[S][i];
+        }
+    }
+    return buf[S];
+}
+
 void fft(bool type)(Complex!double[] c) {
     import std.algorithm : swap;
-    import std.math : PI, sin, cos;
     import core.bitop : bsr;
     alias P = Complex!double;
     size_t N = c.length;
     assert(N);
     size_t S = bsr(N);
     assert(1<<S == N);
-    static P[][30] buf;
-    if (!buf[S].length) {
-        buf[S] = new P[N/2];
-        foreach (i; 0..N/2) {
-            buf[S][i].re = cos(i*2*double(PI)/N);
-            buf[S][i].im = sin(i*2*double(PI)/N);
-        }
+    if (S == 1) {
+        auto x = c[0], y = c[1];
+        c[0] = x+y;
+        c[1] = x-y;
+        return;
     }
-    P[] rot = buf[S];
+    auto rot = fftSinList(S);
     P[] a = c.dup, b = new P[c.length];
     foreach (i; 1..S+1) {
         size_t W = 1<<(S-i);
         for (size_t y = 0; y < N/2; y += W) {
-            P now = rot[y];
+            P now = P(rot[y + N/4], rot[y]);
             if (type) now = conj(now);
             foreach (x; 0..W) {
                 auto l =       a[y<<1 | x];
