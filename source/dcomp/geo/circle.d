459145af7c1afb71a47beaea7ca67569097bb746
Kohei Morita <yosupo06@gmail.com>
2017-12-02 15:53:07 +0900

    add some geo

--- source/dcomp/geo/circle.d
+++ source/dcomp/geo/circle.d
@@ -10,3 +10,21 @@ struct Circre2D(R) {
         this.r = r;
     }
 }
+
+int crossSC(R)(Line2D!R l, Circre2D!R c) {
+    R mi = distSP(l, c.p);
+    if (sgn(mi - c.r) == 1) return 0;
+    if (sgn(c.r - mi) == 0) return 1;
+    int u = 0;
+    if (sgn((l.x-c.p).abs - c.r) != -1) u++;
+    if (sgn((l.y-c.p).abs - c.r) != -1) u++;
+    return u;
+}
+
+unittest {
+    EPS!double = 1e-6;
+    alias P = Point2D!double;
+    auto l = Line2D!double(P(0, 0), P(1, 0));
+    auto c = Circre2D!double(P(3, 3), 1);
+    assert(crossSC(l, c) == 0);
+}
