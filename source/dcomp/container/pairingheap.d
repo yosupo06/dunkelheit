fffea761a7dd015b04d9811f339c739294900802
Kohei Morita <yosupo06@gmail.com>
2017-12-07 22:20:51 +0900

    refact pairingheap

--- source/dcomp/container/pairingheap.d
+++ source/dcomp/container/pairingheap.d
@@ -1,94 +1,113 @@
 module dcomp.container.pairingheap;
 
-NP meldPairingHeapNode(T, alias opCmp, NP)(NP x, NP y) {
+private NP meldPairingHeapNode(alias less, NP)(NP x, NP y) {
     import std.algorithm : swap;
     if (!x) return y;
     if (!y) return x;
-    if (opCmp(x.d, y.d)) swap(x, y);
-    y.next = x.head;
-    x.head = y;
+    if (less(x._item, y._item)) swap(x, y);
+    y._next = x._head;
+    x._head = y;
     return x;
 }
 
-struct PairingHeapPayload(T, alias opCmp) {
-    import std.range : iota;
-    import std.algorithm : swap;
-    alias NP = Node*;
-    static struct Node {        
-        T d;
-        NP head, next;
-        this(T d) {
-            this.d = d;
-        }
-    }
-    NP n;
-    size_t length;
-    bool empty() const { return length == 0; }
+///
+struct PairingHeap(T, alias less = "a < b") {
+    import std.functional : binaryFun;
+    private alias _less = binaryFun!less;
 
-    void insert(T x) {
-        length++;
-        if (!n) n = new Node(x);
-        else n = meldPairingHeapNode!(T, opCmp)(n, new Node(x));
+    private alias NP = Node*;
+    private static struct Node {
+        T _item;
+        NP _head, _next;
+        this(T item) {
+            _item = item;
+        }
     }
+
+    private struct Payload {
+        import std.algorithm : swap;
+        private NP _node;
+        private uint _len;
+
+        void insert(T item) {
+            _len++;
+            _node = meldPairingHeapNode!_less(_node, new Node(item));
+        }
+        inout(T) front() inout { return _node._item; }
+        void removeFront() {
+            _len--;
+
+            NP s = _node._head;
+            NP t;
+            // merge and reverse: (s, s.next, s.next.next, ...)
+            // result: (..., t.next.next, t.next, t)
+            while (s) {
+                // pop first 2 nodes
+                NP first, second;
+                first = s; s = s._next; first._next = null;
+                if (s) {
+                    second = s; s = s._next; second._next = null;
+                }
+                // merge 2 nodes and insert front of t
+                auto v = meldPairingHeapNode!_less(first, second);
+                v._next = t;
+                t = v;
+            }
+            _node = null;
+            // merge t
+            while (t) {
+                NP first = t; t = t._next; first._next = null;
+                _node = meldPairingHeapNode!_less(first, _node);
+            }
+        }
+        void meld(Payload* r) {
+            _len += r._len; r._len = 0;
+            _node = meldPairingHeapNode!_less(_node, r._node);
+            r._node = null;
+        }
+    }
+    private Payload* _p;
+
+    @property bool empty() const { return !_p || _p._len == 0; } ///
+    @property size_t length() const { return (!_p) ? 0 : _p._len; } ///
+
+    void insert(T item) {
+        if (!_p) _p = new Payload();
+        _p.insert(item);
+    } ///
     inout(T) front() inout {
-        assert(n);
-        return n.d;
-    }
+        assert(!empty, "PairingHeap.front: heap is empty");
+        return _p.front;
+    } ///
     void removeFront() {
-        assert(n);
-        assert(length > 0);
-        length--;
-        NP x;
-        NP s = n.head;
-        while (s) {
-            NP a, b;
-            a = s; s = s.next; a.next = null;
-            if (s) {
-                b = s; s = s.next; b.next = null;
-            }
-            a = meldPairingHeapNode!(T, opCmp)(a, b);
-            assert(a);
-            if (!x) x = a;
-            else {
-                a.next = x.next;
-                x.next = a;
-            }
-        }
-        n = null;
-        while (x) {
-            NP a = x; x = x.next;
-            n = meldPairingHeapNode!(T, opCmp)(a, n);
-        }
-    }
+        assert(!empty, "PairingHeap.removeFront: heap is empty");
+        _p.removeFront;
+    } ///
+    /**
+    meld two heaps
+    Warning: r become empty
+    */
+    void meld(PairingHeap r) { _p.meld(r._p); }
 }
 
-struct PairingHeap(T, alias _opCmp) {
-    import std.stdio;
-    import std.functional : binaryFun;
-    alias opCmp = binaryFun!_opCmp;
-    alias Payload = PairingHeapPayload!(T, opCmp);
-    Payload* p;
-    void I() { if (!p) p = new Payload(); }
-    bool empty() const { return !p || p.empty(); }
-    size_t length() const { return (!p) ? 0 : p.length; }
-    void insert(T x) {
-        I();
-        assert(p);
-        p.insert(x);
-    }
-    inout(T) front() inout {
-        assert(p);
-        return p.front;
-    }
-    void removeFront() {
-        assert(p);
-        p.removeFront;
-    }
-    void meld(PairingHeap r) {
-        p.length += r.p.length;
-        p.n = meldPairingHeapNode!(T, opCmp)(p.n, r.p.n);
-        r.p.n = null;
-    }
+///
+unittest {
+    auto p1 = PairingHeap!int();
+    auto p2 = PairingHeap!int();
+
+    p1.insert(1);
+    p1.insert(2);
+    assert(p1.front == 2);
+
+    p2.insert(3);
+    assert(p2.front == 3);
+
+    p1.meld(p2);
+    assert(p1.length == 3 && !p2.length);
+
+    assert(p1.front == 3); p1.removeFront();
+    assert(p1.front == 2); p1.removeFront();
+    assert(p1.front == 1); p1.removeFront();
 }
 
 unittest {
@@ -112,8 +131,8 @@ unittest {
         return sm;
     }
     auto u = Random(unpredictableSeed);
-    import std.stdio;
-    writeln("Start PairingHeap 1000");
+    import dcomp.stopwatch;
+    StopWatch sw; sw.start;
     foreach (i; 0..1000) {
         auto seed = u.front; u.popFront;
         assert(
@@ -121,9 +140,15 @@ unittest {
             f!(BinaryHeap!(Array!int))(Random(seed))
             );
     }
+    import std.stdio;
+    writeln("Start PairingHeap 1000: ", sw.peek.toMsecs);
 }
 
 unittest {
-    auto que = PairingHeap!(int, (a, b) => a < b)();
+    auto cost(int i) {
+        return i;
+    }
+    auto que = PairingHeap!(int, (a, b) => cost(a) < cost(b))();
+    que.insert(10);
+    que.removeFront();
 }
-
