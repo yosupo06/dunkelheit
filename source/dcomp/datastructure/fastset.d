443cd0aac97d3d03d9a09043cd140c857c476943
Kohei Morita <yosupo06@gmail.com>
2017-12-09 19:53:07 +0900

    refact fastset

--- source/dcomp/datastructure/fastset.d
+++ source/dcomp/datastructure/fastset.d
@@ -1,65 +1,60 @@
 module dcomp.datastructure.fastset;
 
+/**
+almost same bool[int], except key range is stricted
+ */
 struct FastSet {
     import std.range : back;
     import core.bitop : bsr, bsf;
-    size_t N, lg;
+    private size_t n, len;
     ulong[][] seg;
-    this(size_t N) {
-        this.N = N;
-        if (N == 0) {
-            seg ~= new ulong[1];
-            return;
-        }
+    /// make set for [0, 1, ..., n-1]
+    this(size_t n) {
+        if (n == 0) n = 1;
+        this.n = n;
         while (true) {
-            seg ~= new ulong[(N+63)/64];
-            if (N == 1) break;
-            N = (N+63)/64;
+            seg ~= new ulong[(n+63)/64];
+            if (n == 1) break;
+            n = (n+63)/64;
         }
-        lg = seg.length;
     }
-    bool empty() const {
-        return seg.back[0] != 0;
-    }
+    bool empty() const { return seg.back[0] != 0; } ///
+    size_t length() const { return len; } /// count true
+
     bool opBinaryRight(string op : "in")(size_t i) {
-        assert(0 <= i && i < N);
+        assert(i < n);
+        //todo: consider bitop.bt
         size_t D = i/64, R = i%64;
         return (seg[0][D] & (1UL << R)) != 0;
-    }
+    } ///
     void insert(size_t x) {
+        if (x in this) return;
+        len++;
         foreach (i; 0..seg.length) {
             size_t D = x/64, R = x%64;
             seg[i][D] |= (1UL << R);
             x /= 64;
         }
-    }
+    } ///
     void remove(size_t x) {
-        foreach (i; 0..seg.length) {
-            size_t D = x/64, R = x%64;
-            seg[i][D] &= ~(1UL << R);
-            if (i && seg[i-1][x] != 0) {
-                seg[i][D] |= (1UL << R);
-            }
+        if (x !in this) return;
+        len--;
+        size_t D = x/64, R = x%64;
+        seg[0][D] &= ~(1UL << R);
+        foreach (i; 1..seg.length) {
             x /= 64;
+            if (seg[i-1][x]) break;
+            D = x/64; R = x%64;
+            seg[i][D] &= ~(1UL << R);
         }
-    }
-    static struct Range {
-        FastSet* fs;
-        size_t lower, upper;
-        size_t front() const {
-            return fs.next(lower);
-        }
-        bool empty() const {
-            return lower >= fs.N || upper <= fs.next(lower);
-        }
-        void popFront() {
-            if (lower < upper) lower = fs.next(lower)+1;
-        }
-    }
-    size_t next(size_t x) const {
-        for (int i = 0; i < lg; i++) {
+    } ///
+    /// return minimum element that isn't less than x
+    ptrdiff_t next(ptrdiff_t x) const {
+        if (x < 0) x = 0;
+        if (n <= x) return n;
+        foreach (i; 0..seg.length) {
+            if (x == seg[i].length * 64) break;
             size_t D = x/64, R = x%64;
-            if (D == seg[i].length) break;
             ulong B = seg[i][D]>>R;
             if (!B) {
                 x = x/64+1;
@@ -67,56 +62,95 @@ struct FastSet {
             }
             //find
             x += bsf(B);
-            for (int j = i-1; j >= 0; j--) {
+            foreach_reverse (j; 0..i) {
                 x *= 64;
-                size_t D2 = x/64;
-                x += bsf(seg[j][D2]);
+                x += bsf(seg[j][x/64]);
             }
             return x;
         }
-        return N;
+        return n;
     }
-    Range lowerBound(size_t x) {
-        return Range(&this, x, N);
+    /// return maximum element that isn't greater than x
+    ptrdiff_t prev(ptrdiff_t x) const {
+        if (n <= x) x = n-1;
+        if (x < 0) return -1;
+        foreach (i; 0..seg.length) {
+            if (x == -1) break;
+            size_t D = x/64, R = x%64;
+            ulong B = seg[i][D]<<(63-R);
+            if (!B) {
+                x = x/64-1;
+                continue;
+            }
+            //find
+            x += bsr(B)-63;
+            foreach_reverse (j; 0..i) {
+                x *= 64;
+                x += bsr(seg[j][x/64]);
+            }
+            return x;
+        }
+        return -1;
     }
-    Range upperBound(size_t x) {
-        return Range(&this, x+1, N);
+    
+    /// return range that contain less than x
+    Range lowerBound(ptrdiff_t x) {
+        return Range(&this, next(0), prev(x-1));
     }
-    // // x以下最大の要素
-    // int back(int x) {
-    //     for (int i = 0; i < lg; i++) {
-    //         if (x == -1) break;
-    //         int D = x/64, R = x%64;
-    //         ull B = seg[i][D]<<(63-R);
-    //         if (!B) {
-    //             x = x/64-1;
-    //             continue;
-    //         }
-    //         //find
-    //         x += bsr(B)-63;
-    //         for (int j = i-1; j >= 0; j--) {
-    //             x *= 64;
-    //             int D = x/64;
-    //             x += bsr(seg[j][D]);
-    //         }
-    //         return x;
-    //     }
-    //     return -1;
-    // }    
+    /// return range that contain greater than x
+    Range upperBound(ptrdiff_t x) {
+        return Range(&this, next(x+1), prev(n-1));
+    }
+    /// 
+    Range opIndex() {
+        return Range(&this, next(0), prev(n-1));
+    }
+    /// bidirectional range
+    static struct Range {
+        FastSet* fs;
+        ptrdiff_t lower, upper;
+
+        @property bool empty() const { return upper < lower; }
+
+        size_t front() const { return lower; }
+        size_t back() const { return upper; }
+        void popFront() {
+            assert(!empty);
+            lower = fs.next(lower+1);
+        }
+        void popBack() {
+            assert(!empty);
+            upper = fs.prev(upper-1);
+        }
+    }
 }
 
+///
 unittest {
+    import std.algorithm : equal, map;
+    import std.range : iota;
     auto fs = FastSet(10);
     fs.insert(1);
     fs.insert(5);
     fs.insert(6);
     fs.remove(5);
+    fs.insert(4);
+    // [1, 4, 6]
     assert(1 in fs);
     assert(2 !in fs);
     assert(5 !in fs);
     assert(6 in fs);
-    import std.algorithm : equal;
-    assert(equal([1, 6], fs.lowerBound(1)));
-    assert(equal([6], fs.upperBound(1)));
+    assert(equal([1, 4, 6], fs[]));
+    assert(equal(
+        iota(8).map!(i => fs.next(i)),
+        [1, 1, 4, 4, 4, 6, 6, 10]
+    ));
+    assert(equal(
+        iota(8).map!(i => fs.prev(i)),
+        [-1, 1, 1, 1, 4, 4, 6, 6]
+    ));
+    assert(equal([1], fs.lowerBound(4)));
+    assert(equal([1, 4], fs.lowerBound(5)));
+    assert(equal([1, 4, 6], fs.upperBound(0)));
+    assert(equal([4, 6], fs.upperBound(1)));
 }
-
