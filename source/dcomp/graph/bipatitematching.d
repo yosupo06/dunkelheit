117b7ff2506637ec3979cf25ddd51d42ff61341d
Kohei Morita <yosupo06@gmail.com>
2017-12-13 21:26:32 +0900

    wow

--- source/dcomp/graph/bipatitematching.d
+++ source/dcomp/graph/bipatitematching.d
@@ -1,12 +1,13 @@
 module dcomp.graph.bipatitematching;
 
 struct BipatiteMatching {
+    import std.conv : to;
     int L, R, count;
     int[][] g;
     int[] lmt, rmt;
     int[] visited; int vc;
-    this(int L, int R) {
-        this.L = L; this.R = R;
+    this(size_t L, size_t R) {
+        this.L = L.to!int; this.R = R.to!int;
         g = new int[][L];
         visited = new int[L];
         lmt = new int[L]; lmt[] = -1;
@@ -25,7 +26,7 @@ struct BipatiteMatching {
         }
         return false;
     }
-    void exec() {
+    void maximize() {
         vc++;
         foreach (i; 0..L) {
             if (lmt[i] != -1) continue;
@@ -33,31 +34,46 @@ struct BipatiteMatching {
             if (dfs(i)) count++;
         }
     }
-    void add(int l, int r) {
-        g[l] ~= r;
-        exec();
+    /// add edge(l, r)
+    void addEdge(size_t l, size_t r) {
+        g[l] ~= r.to!int;
+        maximize();
     }
-    void del(int l, int r) {
+    /// del edge(l, r)
+    void delEdge(size_t l, size_t r) {
         import std.algorithm : remove;
-        g[l] = g[l].remove!(a => a == r);
-        if (rmt[r] == l) {
+        g[l] = g[l].remove!(a => a == r.to!int);
+        if (rmt[r] == l.to!int) {
             count--;
             lmt[l] = rmt[r] = -1;
-            exec();
+            maximize();
         }
     }
-    void addL(int l, int[] v) {
+    /// cng left_l's edge
+    void cngLeftVertexEdge(size_t l, in int[] v) {
         g[l] = v.dup;
         vc++;
-        if (dfs(l)) count++;
-    }
-    void delL(int l) {
-        g[l] = [];
-        if (lmt[l] != -1) {
-            int r = lmt[l];
+        if (lmt[l] == -1) {
+            if (dfs(l.to!int)) count++;
+        } else {
             count--;
+            int r = lmt[l];
             lmt[l] = rmt[r] = -1;
-            exec();
+            maximize();
         }
     }
+}
+
+///
+unittest {
+    auto bm = BipatiteMatching(3, 3);
+    bm.addEdge(0, 0);
+    bm.addEdge(0, 1);
+    bm.addEdge(1, 1);
+    bm.addEdge(2, 1);
+    assert(bm.count == 2); // example: (0, 0), (1, 1)
+    bm.addEdge(1, 2);
+    assert(bm.count == 3); // (0, 0), (1, 2), (2, 1)
+    bm.delEdge(2, 1);
+    assert(bm.count == 2); // example: (0, 0) (1, 2)
 }
